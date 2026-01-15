f8a02c9212bf900fd5e935901f02eb8f5ce2a933
DoughSec <addisondoseck@gmail.com>
2026-01-15 10:21:54 -0500

    Finished control flow

---
+++ js/ControlFlow.js
@@ -0,0 +1,59 @@
+//The area in which the plants are contained is circular, with a radius of 5 meters.
+let radius = 5;
+
+//The formula for calculating the area of a circle is PI multiplied by the radius, squared:
+const PI = 3.1415;
+const area = PI * radius * radius;
+
+//Each plant requires a minimum space of 0.8 square meters.
+const spacePerPlant = 0.8;
+
+//The area is starting with 20 plants.
+let numberOfPlants = 20;
+
+//The plants double in number every week.
+const growthRate = 2;
+
+
+/*
+    Predict the plant growth after a specific number of weeks.
+    Implement control flow to make decisions on whether the plants should be:
+*/
+/*
+    Pruned, to stop them from exceeding the capacity of the garden: 
+    This condition should be met if the plant count after the given number of weeks is greater 
+    than 80% of the maximum capacity of the garden.
+*/
+/*
+    Monitored, if they are growing at an acceptable rate.
+    This condition should be met if the plant count is between 50% and 80% of the
+    maximum capacity of the garden after the given number of weeks.
+*/
+/*
+    Planted, if there is room to plant more plants.
+    This condition should be met if the plant count after the given number of 
+    weeks is less than 50% of the maximum capacity of the garden.
+*/
+
+const maxCapacity = area / spacePerPlant;
+function predictPlantGrowth(weeks) {
+    //Calculate future plant count, exponentially grows not linearly
+    let futurePlantCount = numberOfPlants * Math.pow(growthRate, weeks);
+    //if numberOfPlants > 0.8 * maxCapacity then Pruned
+    if (futurePlantCount > 0.8 * maxCapacity) {
+        return "Pruned";
+        //if numberOfPlants > 0.8 * maxCapacity then Monitored
+    } else if (futurePlantCount >= 0.5 * maxCapacity) {
+        return "Monitored";
+        //if numberOfPlants > 0.8 * maxCapacity then Planted
+    } else {
+        return "Planted";
+    }
+}
+
+//Results after 1 week
+console.log(predictPlantGrowth(1));
+//Results after 2 weeks
+console.log(predictPlantGrowth(2));
+//Results after 3 weeks
+console.log(predictPlantGrowth(3));
